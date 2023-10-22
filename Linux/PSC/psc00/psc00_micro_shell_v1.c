1a6121fb935d8f08e72f079ec40986dd4577cf64
rohith161 <rajrohith161@gmail.com>
2023-10-22 11:43:31 +0530

    updated psc00

--- Linux/PSC/psc00/psc00_micro_shell_v1.c
+++ Linux/PSC/psc00/psc00_micro_shell_v1.c
@@ -15,9 +15,18 @@ int microShellV1(){
     printf("\n\t\t--- Welcome to Micro Shell - V1 ---\n\n\n");
     char input[100];
     int status;
-        printf("%s", "MicroShell] ");
-        fgets(input, sizeof(input), stdin);
-        input[strcspn(input, "\n")] = 0;
+       int byteread = 0;
+        byteread = write(1, "MicroShell] ",strlen("MicroShell] "));
+        if(byteread < 0){
+            perror("write");
+            return errno;
+        }
+        byteread = read(0,input,sizeof(input));
+        if(byteread < 0){
+            perror("read");
+            return errno;
+        }
+        input[byteread-1] = '\0';
 
         int ret = fork();
 
